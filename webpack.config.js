2870b5f5a3b8e80208286f6eacf712251185076f
nikoladjekic <nikoladjekic.dev@gmail.com>
2024-03-23 12:14:14 +0100

    install svgr/webpack and file-loader to show icons as react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,62 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    port: '4004',
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    hot: true,
-    open: true,
-  },
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: "@svgr/webpack",
+						options: {
+							svgoConfig: {
+								plugins: [
+									{ name: "removeViewBox", active: false },
+								],
+							},
+						},
+					},
+					{
+						loader: "file-loader",
+						options: {
+							name: "[path][name].[ext]",
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+	],
+	devServer: {
+		port: "4004",
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		hot: true,
+		open: true,
+	},
 };
